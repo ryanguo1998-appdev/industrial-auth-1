87bd92e4569a424d98d23a034075297f3a9a06bb
ryanguo1998 <rguo98@uchicago.edu>
2022-04-19 21:51:35 +0000

    feed and discover hidden from non-user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,18 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show liked feed followers following discover ]
   before_action :ensure_user_is_follower, only: %i[ liked ]
+  before_action :ensure_user_is_current_user, only: %i[ feed discover ]
 
   def ensure_user_is_follower
     if @user.followers.exclude? current_user
       p "got here #{current_user.inspect}"
       redirect_back fallback_location: user_path(@user.username), alert: "Sending you back" 
+    end
+  end
+
+  def ensure_user_is_current_user
+    if @user != current_user
+      redirect_back fallback_location: root_path, alert: "You can't do that"
     end
   end
 
