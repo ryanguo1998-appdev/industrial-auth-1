c3f8bdaa666768fe72fa99180e640b8eb322c7f1
ryanguo1998 <86497838+ryanguo1998@users.noreply.github.com>
2022-04-19 16:15:37 -0500

    redirect done, user cannot see feed/liked of user they dont follow (#3)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,20 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[ show liked feed followers following discover ]
+  before_action :ensure_user_is_follower, only: %i[ liked ]
+
+  def ensure_user_is_follower
+    if @user.followers.exclude? current_user
+      p "got here #{current_user.inspect}"
+      redirect_back fallback_location: user_path(@user.username), alert: "Sending you back" 
+    end
+  end
 
   private
 
     def set_user
       if params[:username]
         @user = User.find_by!(username: params.fetch(:username))
+        p @user
       else
         @user = current_user
       end
